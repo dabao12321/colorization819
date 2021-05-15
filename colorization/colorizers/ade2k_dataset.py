23afedb129840d2f459de622940c0a2f9b0302ed
EC2 Default User <ec2-user@ip-172-31-86-194.ec2.internal>
2021-05-15 07:30:24 +0000

    fixed bug with greyscale inputs breaking dataloader

--- colorization/colorizers/ade2k_dataset.py
+++ colorization/colorizers/ade2k_dataset.py
@@ -50,18 +50,17 @@ class ADE2kDataset(torch.utils.data.Dataset):
         # print("mask path", mask_path)
 
         img = Image.open(img_path)
+        img = img.convert("RGB")
         mask = Image.open(mask_path)
+
         img_l_orig, img_l_rs, img_ab_rs = preprocess_img(np.asarray(img), HW=(256,256), resample=Image.NEAREST, ade2k=True)
         mask_rs = mask.resize((256, 256), resample=Image.NEAREST)
-
 
         # need to output 3 things (2 things for now)
         # 1. ade2k image bw resized
         # 2. ade2k mask resized, ignore for now
         # 3. ade2k 1 x 2 x 256 x 256 image ab tensor
         return img_l_rs, img_ab_rs
-        
-
 
 if __name__=="__main__":
     # print(sys.path)
@@ -105,94 +104,80 @@ if __name__=="__main__":
         running_loss = 0.0
         epoch_loss = 0.0
         epoch_path = model_dir + "epoch_" + str(epoch) + ".pt"
-        bad_file_indexes = []
         for i, data in enumerate(trainloader, 0):
-            try:
-                # get the inputs; data is a list of [inputs, labels]
-                inputs, labels = data
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
 
-                # print("input size after data", list(inputs.size()))
-                # flattening input and label due to batch size = 1 (dataloader adds dim for batch)
-                inputs = torch.squeeze(inputs, 1)
-                labels = torch.squeeze(labels, 1)
-                # print("input size after squeeze", list(inputs.size()))
+            # print("input size after data", list(inputs.size()))
+            # flattening input and label due to batch size = 1 (dataloader adds dim for batch)
+            inputs = torch.squeeze(inputs, 1)
+            labels = torch.squeeze(labels, 1)
+            # print("input size after squeeze", list(inputs.size()))
 
 
-                inputs = inputs.to(device)
-                # print("input size after moving", list(inputs.size()))
-                labels = labels.to(device)
+            inputs = inputs.to(device)
+            # print("input size after moving", list(inputs.size()))
+            labels = labels.to(device)
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
-                # print("input size after opt", list(inputs.size()))
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # print("input size after opt", list(inputs.size()))
 
-                # forward + backward + optimize
+            # forward + backward + optimize
 
-                # print("my input is size", list(inputs.size()))
-                outputs = model(inputs)
-                loss = criterion(outputs, labels)
-                loss.backward()
-                optimizer.step()
+            # print("my input is size", list(inputs.size()))
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-                # print statistics
-                running_loss += loss.item()
-                epoch_loss += loss.item()
-                if i % 6 == 0:    # print every 6 batches of batch = 16, 96 samples
-                    print('\t[%d, %5d] loss: %.3f' %
-                        (epoch + 1, i*batch, running_loss / 6))
-                    running_loss = 0.0
-            except ValueError:
-                print("ValueError occurred at index", i)
-                bad_file_indexes.append(i)
+            # print statistics
+            running_loss += loss.item()
+            epoch_loss += loss.item()
+            if i % 6 == 0:    # print every 6 batches of batch = 16, 96 samples
+                print('\t[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i*batch, running_loss / 6))
+                running_loss = 0.0
 
 
         print("Epoch", epoch, "complete, saving to...", epoch_path)
         torch.save(model.state_dict(), epoch_path)
-        epoch_loss_avg = epoch_loss/20210
+        epoch_loss_avg = epoch_loss/(20210/batch)
         print("Saved epoch", epoch, "avg epoch loss = ", epoch_loss_avg)
 
         print("Running validation...")
         val_loss = 0.0
         running_val_loss = 0.0
-        bad_file_indexes_val = []
         for i, data in enumerate(valloader, 0):
-            try:
-                # get the inputs; data is a list of [inputs, labels]
-                inputs, labels = data
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
 
-                # print("input size after data", list(inputs.size()))
-                # flattening input and label due to batch size = 1 (dataloader adds dim for batch)
-                inputs = torch.squeeze(inputs, 1)
-                labels = torch.squeeze(labels, 1)
-
-
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # only run forward for val
-                outputs = model(inputs)
-
-                # print statistics
-                running_val_loss += loss.item()
-                val_loss += loss.item()
-                if i % 6 == 0:    # print every 6 batches of batch = 16, 96 samples
-                    print('\t[%d, %5d] loss: %.3f' %
-                        (epoch + 1, i*batch, running_val_loss / 6))
-                    running_val_loss = 0.0
-
-            except ValueError:
-                print("Bad validation file at batch ", i)
-                bad_file_indexes_val.append(i)
+            # print("input size after data", list(inputs.size()))
+            # flattening input and label due to batch size = 1 (dataloader adds dim for batch)
+            inputs = torch.squeeze(inputs, 1)
+            labels = torch.squeeze(labels, 1)
 
 
-        print("Epoch", epoch, "validation loss:", val_loss/2000)
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # only run forward for val
+            outputs = model(inputs)
+
+            # print statistics
+            running_val_loss += loss.item()
+            val_loss += loss.item()
+            if i % 6 == 0:    # print every 6 batches of batch = 16, 96 samples
+                print('\t[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i*batch, running_val_loss / 6))
+                running_val_loss = 0.0
+
+        print("Epoch", epoch, "validation loss:", val_loss/(2000/batch))
         with open("model_weights/val_loss_" + str(epoch) +".txt", "w") as f:
-            f.write("Epoch " + str(epoch) + " validation loss: " + str(val_loss/2000))
-            f.write("bad_file_indexes batch for training: " + str(bad_file_indexes))
-            f.write("bad_file_indexes batch for val: " + str(bad_file_indexes_val))
+            f.write("Epoch " + str(epoch) + " validation loss: " + str(val_loss/(2000/batch)))
             f.close()
 
 
