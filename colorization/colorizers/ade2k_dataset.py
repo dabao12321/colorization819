fixed loss calculation?

colorization/colorizers/ade2k_dataset.py
@@ -67,12 +67,17 @@ class ADE2kDataset(torch.utils.data.Dataset):
         return img_l_rs, img_ab_rs
 
 if __name__=="__main__":
+    ab_norm = 110.
+    ab_max = 110.
+    ab_quant = 10.
+    A = 2 * ab_max / ab_quant + 1
+
     # print(sys.path)
-    train_dataset = ADE2kDataset("/home/ec2-user/colorization819/colorization/data/ADEChallengeData2016/images/training", \
-                            "/home/ec2-user/colorization819/colorization/data/ADEChallengeData2016/annotations/training", \
+    train_dataset = ADE2kDataset("../data/ADEChallengeData2016/images/training", \
+                            "../data/ADEChallengeData2016/annotations/training", \
                             "train")
-    val_dataset = ADE2kDataset("/home/ec2-user/colorization819/colorization/data/ADEChallengeData2016/images/validation", \
-                            "/home/ec2-user/colorization819/colorization/data/ADEChallengeData2016/annotations/validation", \
+    val_dataset = ADE2kDataset("../data/ADEChallengeData2016/images/validation", \
+                            "../data/ADEChallengeData2016/annotations/validation", \
                             "val")
     
     # Params
@@ -88,7 +93,8 @@ if __name__=="__main__":
 
     # Moving model to GPU
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model.cuda()
+    if torch.cuda.is_available():
+        model.cuda()
 
     print("checkpt 2")
 
@@ -99,8 +105,9 @@ if __name__=="__main__":
     print("checkpt 3")
 
     # Loss and optimizer
-    criterion = torch.nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    criterionCE = torch.nn.CrossEntropyLoss()
+    criterionL1 = torch.nn.L1Loss()
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 
     print("checkpt 4")
 
@@ -116,20 +123,23 @@ if __name__=="__main__":
         else:
             for i, data in enumerate(trainloader, 0):
                 # get the inputs; data is a list of [inputs, labels]
-                inputs, labels, masks = data
-
-                # print("masks size after data", list(masks.size()))
+                l_inputs, ab_inputs, masks = data
+                
+                # print("input size after data", list(inputs.size()))
                 # flattening input and label due to batch size = 1 (dataloader adds dim for batch)
-                inputs = torch.squeeze(inputs, 1)
-                labels = torch.squeeze(labels, 1)
+                l_inputs = torch.squeeze(l_inputs, 1)
+                ab_inputs = torch.squeeze(ab_inputs, 1)
                 masks = torch.squeeze(masks, 1)
                 # print("input size after squeeze", list(inputs.size()))
 
-
-                inputs = inputs.to(device)
+                l_inputs = l_inputs.to(device)
                 # print("input size after moving", list(inputs.size()))
-                labels = labels.to(device)
+                ab_inputs = ab_inputs.to(device)
                 masks = masks.to(device)
+
+                # print("lshape", l_inputs.shape)
+                # print("abshape", ab_inputs.shape)
+                ab_rs = ab_inputs[:, :, ::4, ::4]
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
@@ -138,8 +148,24 @@ if __name__=="__main__":
                 # forward + backward + optimize
 
                 # print("my input is size", list(inputs.size()))
-                outputs = model(inputs, mask_B=masks)
-                loss = criterion(outputs, labels)
+                (reg_outputs, class_outputs) = model(l_inputs, mask_B = masks)
+                ab_enc = encode_ab_ind(ab_rs, ab_max, ab_quant, A)
+                class_loss = 0
+                # for i in range(batch):
+                #     print("shape1", class_outputs.type(torch.FloatTensor)[i, :, :, :].shape)
+                #     print("shape2", ab_enc.type(torch.LongTensor)[i, :, :].shape)
+                #     loss += criterion(class_outputs.type(torch.FloatTensor)[i, :, :, :], ab_enc.type(torch.LongTensor)[i, :, :])
+                # print("shape1", class_outputs.type(torch.FloatTensor).shape)
+                # print("shape2", ab_enc[:, 0, :, :].type(torch.LongTensor).shape)
+                if torch.cuda.is_available():
+                    class_loss += criterionCE(class_outputs.type(torch.cuda.FloatTensor), ab_enc[:, 0, :, :].type(torch.cuda.LongTensor))
+                else:
+                    class_loss += criterionCE(class_outputs.type(torch.FloatTensor), ab_enc[:, 0, :, :].type(torch.LongTensor))
+                ab_inputs = ab_inputs / ab_norm
+                reg_loss = 10 * torch.mean(criterionL1(reg_outputs.type(torch.cuda.FloatTensor),
+                                                              ab_inputs.type(torch.cuda.FloatTensor)))
+                loss = class_loss * 1. + reg_loss
+                print("test class vs reg:", class_loss.item(), reg_loss.item())
                 loss.backward()
                 optimizer.step()
 
@@ -164,21 +190,49 @@ if __name__=="__main__":
         with torch.no_grad():
             for i, data in enumerate(valloader, 0):
                 # get the inputs; data is a list of [inputs, labels]
-                inputs, labels, masks = data
-
+                l_inputs, ab_inputs, masks = data
+                
                 # print("input size after data", list(inputs.size()))
                 # flattening input and label due to batch size = 1 (dataloader adds dim for batch)
-                inputs = torch.squeeze(inputs, 1)
-                labels = torch.squeeze(labels, 1)
+                l_inputs = torch.squeeze(l_inputs, 1)
+                ab_inputs = torch.squeeze(ab_inputs, 1)
                 masks = torch.squeeze(masks, 1)
+                # print("input size after squeeze", list(inputs.size()))
 
-                inputs = inputs.to(device)
-                labels = labels.to(device)
+                l_inputs = l_inputs.to(device)
+                # print("input size after moving", list(inputs.size()))
+                ab_inputs = ab_inputs.to(device)
                 masks = masks.to(device)
 
-                # only run forward for val
-                outputs = model(inputs, mask_B=masks)
-                loss = criterion(outputs, labels)
+                # print("lshape", l_inputs.shape)
+                # print("abshape", ab_inputs.shape)
+                ab_rs = ab_inputs[:, :, ::4, ::4]
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                # print("input size after opt", list(inputs.size()))
+
+                # forward + backward + optimize
+
+                # print("my input is size", list(inputs.size()))
+                (reg_outputs, class_outputs) = model(l_inputs, mask_B = masks)
+                ab_enc = encode_ab_ind(ab_rs, ab_max, ab_quant, A)
+                class_loss = 0
+                # for i in range(batch):
+                #     print("shape1", class_outputs.type(torch.FloatTensor)[i, :, :, :].shape)
+                #     print("shape2", ab_enc.type(torch.LongTensor)[i, :, :].shape)
+                #     loss += criterion(class_outputs.type(torch.FloatTensor)[i, :, :, :], ab_enc.type(torch.LongTensor)[i, :, :])
+                # print("shape1", class_outputs.type(torch.FloatTensor).shape)
+                # print("shape2", ab_enc[:, 0, :, :].type(torch.LongTensor).shape)
+                if torch.cuda.is_available():
+                    class_loss += criterionCE(class_outputs.type(torch.cuda.FloatTensor), ab_enc[:, 0, :, :].type(torch.cuda.LongTensor))
+                else:
+                    class_loss += criterionCE(class_outputs.type(torch.FloatTensor), ab_enc[:, 0, :, :].type(torch.LongTensor))
+                ab_inputs = ab_inputs / ab_norm
+                reg_loss = 10 * torch.mean(criterionL1(reg_outputs.type(torch.cuda.FloatTensor),
+                                                              ab_inputs.type(torch.cuda.FloatTensor)))
+                loss = class_loss * 1. + reg_loss
+                print("val class vs reg:", class_loss.item(), reg_loss.item())
 
                 # print statistics
                 running_val_loss += loss.item()

colorization/colorizers/siggraph17.py
@@ -1,11 +1,15 @@
 import torch
 import torch.nn as nn
+import sklearn.neighbors as neighbors
 
 from .base_color import *
+from .util import *
 
 class SIGGRAPHGenerator(BaseColor):
     def __init__(self, norm_layer=nn.BatchNorm2d, classes=529):
         super(SIGGRAPHGenerator, self).__init__()
+
+        # model_ss = [nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=8, groups=2)]
 
         # Conv1
         model1=[nn.Conv2d(4, 64, kernel_size=3, stride=1, padding=1, bias=True),]
@@ -69,7 +73,7 @@ class SIGGRAPHGenerator(BaseColor):
         model7+=[nn.ReLU(True),]
         model7+=[norm_layer(512),]
 
-        # Conv7
+        # Conv8
         model8up=[nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=True)]
         model3short8=[nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),]
 
@@ -99,6 +103,9 @@ class SIGGRAPHGenerator(BaseColor):
         model10+=[nn.Conv2d(128, 128, kernel_size=3, dilation=1, stride=1, padding=1, bias=True),]
         model10+=[nn.LeakyReLU(negative_slope=.2),]
 
+        # Color encoding
+        # model_colorencode = [NeighborsEncLayer(),]
+
         # classification output
         model_class=[nn.Conv2d(256, classes, kernel_size=1, padding=0, dilation=1, stride=1, bias=True),]
 
@@ -123,13 +130,17 @@ class SIGGRAPHGenerator(BaseColor):
         self.model2short9 = nn.Sequential(*model2short9)
         self.model1short10 = nn.Sequential(*model1short10)
 
+        # self.model_colorencode = nn.Sequential(*model_colorencode)
+        # is this the right place to call this?
+        # self.model_colorencode.zero_grad()
+
         self.model_class = nn.Sequential(*model_class)
         self.model_out = nn.Sequential(*model_out)
 
         self.upsample4 = nn.Sequential(*[nn.Upsample(scale_factor=4, mode='bilinear', align_corners=True),])
         self.softmax = nn.Sequential(*[nn.Softmax(dim=1),])
 
-    def forward(self, input_A, input_B=None, mask_B=None):
+    def forward(self, input_A, input_B=None, mask_B=None, output=False):
         if(input_B is None):
             input_B = torch.cat((input_A*0, input_A*0), dim=1)
         if(mask_B is None):
@@ -152,25 +163,65 @@ class SIGGRAPHGenerator(BaseColor):
         conv6_3 = self.model6(conv5_3)
         conv7_3 = self.model7(conv6_3)
 
+        conv1_2 = self.model1(torch.cat((input_A, input_B, mask_B), dim=1))
+        conv2_2 = self.model2(conv1_2[:, :, ::2, ::2])
+        conv3_3 = self.model3(conv2_2[:, :, ::2, ::2])
+        conv4_3 = self.model4(conv3_3[:, :, ::2, ::2])
+        conv5_3 = self.model5(conv4_3)
+        conv6_3 = self.model6(conv5_3)
+        conv7_3 = self.model7(conv6_3)
         conv8_up = self.model8up(conv7_3) + self.model3short8(conv3_3)
         conv8_3 = self.model8(conv8_up)
-        conv9_up = self.model9up(conv8_3) + self.model2short9(conv2_2)
+
+        # if(self.classification):
+        out_class = self.model_class(conv8_3)
+
+        conv9_up = self.model9up(conv8_3.detach()) + self.model2short9(conv2_2.detach())
         conv9_3 = self.model9(conv9_up)
-        conv10_up = self.model10up(conv9_3) + self.model1short10(conv1_2)
+        conv10_up = self.model10up(conv9_3) + self.model1short10(conv1_2.detach())
         conv10_2 = self.model10(conv10_up)
         out_reg = self.model_out(conv10_2)
+        # else:
+        # out_class = self.model_class(conv8_3.detach())
 
-        conv9_up = self.model9up(conv8_3) + self.model2short9(conv2_2)
-        conv9_3 = self.model9(conv9_up)
-        conv10_up = self.model10up(conv9_3) + self.model1short10(conv1_2)
-        conv10_2 = self.model10(conv10_up)
-        out_reg = self.model_out(conv10_2)
+        # conv9_up = self.model9up(conv8_3) + self.model2short9(conv2_2)
+        # conv9_3 = self.model9(conv9_up)
+        # conv10_up = self.model10up(conv9_3) + self.model1short10(conv1_2)
+        # conv10_2 = self.model10(conv10_up)
+        # out_reg = self.model_out(conv10_2)
 
-        return self.unnormalize_ab(out_reg)
+        # print("83", conv8_3.shape)
+        # print("8up", conv9_up.shape)
+        # print("93", conv9_3.shape)
+        # print("10up", conv10_up.shape)
+        # print("102", conv10_2.shape)
+        # print("outreg", out_reg.shape)
+
+        """
+        colorencode = self.model_colorencode(conv8_3)
+        data_ab_ss = self.model_ab_ss(input_B)
+        gt_ab_ss = self.model_colorencode(data_ab_ss)
+        nongray_mask = self.model_nongraymask(data_ab_ss)
+        prior_boost_nongray = prior_boost * nongray_mask
+
+        conv8_boost = self.class_rebalance(conv8_3, prior_boost_nongray)
+        # TODO: what
+        class_reg = self.model_class(conv8_boost)
+
+        return self.unnormalize_ab(class_reg)
+        """
+        # print("outclass", out_class.shape)
+        if (output):
+            return (out_reg, out_class, conv8_3)
+
+        # Don't unnormalize: compare to normalized ab values
+        return(out_reg, out_class)
 
 def siggraph17(pretrained=True):
     model = SIGGRAPHGenerator()
-    if(pretrained):
+    if (pretrained):
         import torch.utils.model_zoo as model_zoo
         model.load_state_dict(model_zoo.load_url('https://colorizers.s3.us-east-2.amazonaws.com/siggraph17-df00044c.pth',map_location='cpu',check_hash=True))
+    # else:
+    #     model.load_state_dict(torch.load(epoch_path), map_location='cpu')
     return model